8db3b5f05b482bbc11c0615891219bae5891aa68
tpham20908 <tpham20908@gmail.com>
2018-06-23 09:10:16 -0400

    customed navbar and footer

--- LightLib/App_Start/BundleConfig.cs
+++ LightLib/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace LightLib
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrapspacelab.css",
                       "~/Content/site.css"));
         }
     }
